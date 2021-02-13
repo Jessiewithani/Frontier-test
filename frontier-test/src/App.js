4b1e76f62f252d80a2edd3c6dbe4bd6d825cca68
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-02-12 18:42:14 -0700

    Pseudo code initial steps

--- frontier-test/src/App.js
+++ frontier-test/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+//START TIME: 6:30pm after wireframe
+
+// ONE: 
+
+// first I created a react app
+// then I took some time to do a wireframe of the overall application
+// there are 7 components all together
+// 1. app
+// 2. header
+// 3. accounts container
+// 4. active accounts container
+// 5. overdue accounts container
+// 6. inactive accounts container
+// 7. individual accounts
+
+// TWO: 
+
+// next thing i'm going to have to do is be able to get access to the given endpoint
+// I'm going to turn my App component into a class component, so I can have my state
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ACCOUNTS</h1>
       </header>
     </div>
   );
