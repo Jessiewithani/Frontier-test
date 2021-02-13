7025e8b34e4a88e4274d1acca1c4ba16d42e0743
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-02-12 19:24:19 -0700

    Make account card

---
+++ frontier-test/src/Account.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Account.css';
+
+const Account = () => {
+    return(
+        <div className="account">
+            <h3>account</h3>
+        </div>
+    )
+}
+
+export default Account;
--- frontier-test/src/ActiveContainer.js
+++ frontier-test/src/ActiveContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ActiveContainer.css';
+import Account from './Account';
 
 const ActiveContainer = () => {
     return(
         <div className="active-container">
             <h2>active container</h2>
+            <Account />
         </div>
     )
 }
