78e8882778371f2946f17bc767c1ed3f64d6bb74
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-02-12 18:50:00 -0700

    Fetch data from endpoint and set to state

--- frontier-test/src/App.js
+++ frontier-test/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { Component } from 'react';
 
 //START TIME: 6:30pm after wireframe
 
@@ -20,14 +21,34 @@ import './App.css';
 // next thing i'm going to have to do is be able to get access to the given endpoint
 // I'm going to turn my App component into a class component, so I can have my state
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ACCOUNTS</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      accounts: [],
+      isLoading: true
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('https://frontiercodingtests.azurewebsites.net/api/accounts/getall')
+      .then(res => res.json())
+      .then(data => this.setState({accounts: data}))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    { console.log('accounts', this.state.accounts)}
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ACCOUNTS</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
